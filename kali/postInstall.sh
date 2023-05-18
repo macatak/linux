793ae427d453416d43e55f0abd6cce1b9968f7e8
macatak <mdmclauchlin@gmail.com>
2023-05-18 12:34:14 -0600

    Update postInstall.sh

--- kali/postInstall.sh
+++ kali/postInstall.sh
@@ -13,7 +13,7 @@ curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 # sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list\n
 
 
 # install added repos
