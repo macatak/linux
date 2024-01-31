36afebc5f5e7bb187e4811d57024c0f8d3ab3042
macatak <mdmclauchlin@gmail.com>
2024-01-31 16:42:15 -0700

    Update postInstall.sh

--- kali/postInstall.sh
+++ kali/postInstall.sh
@@ -3,7 +3,9 @@
 sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt -y autoremove
 
 # installs
-sudo apt install -y dnsutils chromium ufw snapd curl gpg software-properties-common apt-transport-https feroxbuster gobuster rlwrap eyewitness bloodhound zaproxy chromium golang
+sudo apt install -y dnsutils chromium ufw snapd curl gpg software-properties-common \
+apt-transport-https feroxbuster gobuster rlwrap eyewitness bloodhound zaproxy \
+chromium golang peass
 
 sudo systemctl start snapd.service
 
