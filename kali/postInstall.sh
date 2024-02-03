e2815db63df40fcf0e297d7177eff07ccf28b790
macatak <mdmclauchlin@gmail.com>
2024-02-03 16:54:03 -0700

    Update postInstall.sh

--- kali/postInstall.sh
+++ kali/postInstall.sh
@@ -1,6 +1,6 @@
 # Update/Upgrade 
 
-sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt -y autoremove
+sudo apt update && sudo apt -y dist-upgrade && sudo apt -y autoremove
 
 # installs
 sudo apt install -y dnsutils chromium ufw snapd curl gpg software-properties-common \
@@ -14,10 +14,7 @@ sudo systemctl start snapd.service
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add 
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 # sublime
-# seems to be broken
-# wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-# echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list\n
-
+sudo snap install sublime
 
 # install added repos
 sudo apt update && sudo apt install -y code # sublime-text
