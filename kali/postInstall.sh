3a89ca37a72f75f27c2c5dd750fb0eb0357a4257
macatak <mdmclauchlin@gmail.com>
2024-02-04 10:05:24 -0700

    Update postInstall.sh
    fixed sublime

--- kali/postInstall.sh
+++ kali/postInstall.sh
@@ -14,7 +14,7 @@ sudo systemctl start snapd.service
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add 
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 # sublime
-sudo snap install sublime
+sudo snap install sublime-text --classic
 
 # install added repos
 sudo apt update && sudo apt install -y code # sublime-text
